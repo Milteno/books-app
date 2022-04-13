3e8403e71a646180ee0c8d787916a00d49a2daec
Grzegorz Przezdziecki <mlgforest6654@gmail.com>
2022-04-13 18:20:10 +0200

    Init project

---
+++ src/js/data.js
@@ -0,0 +1,59 @@
+const dataSource = {}; // eslint-disable-line no-unused-vars
+
+dataSource.books = [
+  {
+    id: 1,
+    name: 'Lady in red',
+    price: 20,
+    rating: 8,
+    image: 'images/books/1.jpg',
+    details: {
+      adults: true,
+      nonFiction: false
+    }
+  },
+  {
+    id: 2,
+    name: 'JS Design Patterns',
+    price: 15,
+    rating: 9.2,
+    image: 'images/books/2.jpg',
+    details: {
+      adults: false,
+      nonFiction: true
+    }
+  },
+  {
+    id: 3,
+    name: 'Eloquent Javascript',
+    image: 'images/books/3.jpg',
+    price: 20,
+    rating: 7.8,
+    details: {
+      adults: false,
+      nonFiction: true
+    }
+  },
+  {
+    id: 4,
+    name: 'You don\'t know JS',
+    image: 'images/books/4.jpg',
+    price: 40,
+    rating: 8.2,
+    details: {
+      adults: false,
+      nonFiction: true
+    }
+  },
+  {
+    id: 5,
+    name: 'Of Mice and Men',
+    image: 'images/books/5.jpg',
+    rating: 6.4,
+    price: 40,
+    details: {
+      adults: false,
+      nonFiction: false
+    }
+  },
+];
---
+++ src/js/functions.js
@@ -0,0 +1,7 @@
+const utils = {}; // eslint-disable-line no-unused-vars
+
+utils.createDOMFromHTML = function(htmlString) {
+  let div = document.createElement('div');
+  div.innerHTML = htmlString.trim();
+  return div.firstChild;
+};
